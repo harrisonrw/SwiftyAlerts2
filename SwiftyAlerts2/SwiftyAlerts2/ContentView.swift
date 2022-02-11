36c04e290d8ca36c7420fd83e4637d6bd6366fbb
Robert Harrison <harrisonrw@gmail.com>
2022-02-11 15:39:06 -0800

    Initial Xcode Project

---
+++ SwiftyAlerts2/SwiftyAlerts2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftyAlerts2
+//
+//  Created by Robert Harrison on 2/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftyAlerts2/SwiftyAlerts2/SwiftyAlerts2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyAlerts2App.swift
+//  SwiftyAlerts2
+//
+//  Created by Robert Harrison on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyAlerts2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
